1c11eabcfefaae063b35e824b1ae2f2c20c69b0d
ahmedelzahr <36417263+ahmedelzahr@users.noreply.github.com>
2025-10-04 13:14:20 +0300

    25-10-04 Seeding posts and users

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use Nette\Utils\Random;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -16,8 +18,12 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        // $images=['1.png','2.png','3.png','4.png','5.png','6.png'];
         return [
-            //
+            'description'=>fake()->sentence(),
+            'slug'=>Str::slug(fake()->sentence(6)),
+            // 'image'=>'posts/'.array_rand($images)
+            'image'=>'posts/'.rand(1,6).'.png'
         ];
     }
 }
